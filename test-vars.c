e939e66b6beea9721042f3f757ddf6b5e16e4e8c
James Cuzella <james.cuzella@lyraphase.com>
2022-02-01 09:43:12 -0700

    Adding basic data types program

---
+++ test-vars.c
@@ -0,0 +1,16 @@
+/*
+* Author: James Cuzella
+* Purpose: This program uses basic data types
+* 
+*/
+#include <stdio.h>
+
+int main(void) {
+  int integerVar = 100;
+  float floatingVar = 331.79;
+  double doubleVar = 8.44e+11;
+
+  _Bool boolVar = 0;
+
+  return 0;
+}
