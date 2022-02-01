16a63db9f8210af4f407db31cabada5f4ccb526e
James Cuzella <james.cuzella@lyraphase.com>
2022-02-01 16:10:23 -0700

    Add demo program for CIDR range divisibility

---
+++ cidr_ranges.c
@@ -0,0 +1,25 @@
+/*
+* Author: James Cuzella
+* Purpose: This program calculates which CIDR IPv4 ranges
+*          are divisible by N for (0 <= N <= 16)
+* 
+*/
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+
+  bool is_divisible = false;
+  int cidr_ips = 0;
+  
+  for (int i=0; i <= 16; i++) {
+    cidr_ips = 1 << i;
+    for (int j=0; j <= 16; j++) {
+      bool is_divisible = (cidr_ips % j == 0);
+      if (is_divisible) {
+        printf("2^%d CIDR with %d IPs is divisible by %d\n", i, cidr_ips, j);
+      }
+    }
+  }
+  return 0;
+}
